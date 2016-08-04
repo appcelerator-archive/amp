f6cffa7d9f0f84da8076297539df5b0e0f34efef
bq <bquenin@axway.com>
2016-08-04 15:31:44 +0200

    Working on etcd integration

--- api/server/project.go
+++ api/server/project.go
@@ -1,27 +1,13 @@
 package server
 
 import (
-	"fmt"
-
-	"encoding/json"
-
 	"github.com/appcelerator/amp/api/rpc/project"
 
 	"golang.org/x/net/context"
 )
 
 const (
-	esIndex   = "amp-project"
-	esType    = "project"
-	esMapping = `{
-			"project":{
-				"properties":{
-					"name":{
-						"type":"string"
-					}
-				}
-			}
-		}`
+	etcdKeyPrefix = "/amp/project"
 )
 
 // projectService is used to implement project.ProjectServer
@@ -30,23 +16,23 @@ type projectService struct {
 
 // CreateProject implements project.ProjectServer
 func (s *projectService) Create(ctx context.Context, in *project.CreateRequest) (*project.CreateReply, error) {
-	// Indexing the project
-	es.Index(esIndex, esType, in)
+	// Storing the project
+	etc.SetKey(etcdKeyPrefix, in)
 
-	// Get all the projects
-	all := es.All(esIndex)
-
-	// Iterate through results
-	for _, hit := range all {
-		// Deserialize hit.Source into a CreateRequest (could also be just a map[string]interface{}).
-		var cr project.CreateRequest
-		err := json.Unmarshal(*hit.Source, &cr)
-		cr.Id = hit.Id
-		if err != nil {
-			// Deserialization failed
-		}
-		fmt.Printf("Project: %+v\n", cr)
-	}
+	//// Get all the projects
+	//all := es.All(esIndex)
+	//
+	//// Iterate through results
+	//for _, hit := range all {
+	//	// Deserialize hit.Source into a CreateRequest (could also be just a map[string]interface{}).
+	//	var cr project.CreateRequest
+	//	err := json.Unmarshal(*hit.Source, &cr)
+	//	cr.Id = hit.Id
+	//	if err != nil {
+	//		// Deserialization failed
+	//	}
+	//	fmt.Printf("Project: %+v\n", cr)
+	//}
 
 	return &project.CreateReply{Message: "Hello " + in.Name}, nil
 }
