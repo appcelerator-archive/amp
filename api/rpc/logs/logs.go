278cbe3b30a686b95dfde74990800150901d99b1
bq <bquenin@axway.com>
2016-08-12 16:37:42 +0200

    Adding filtering parameters to server implementation

--- api/rpc/logs/logs.go
+++ api/rpc/logs/logs.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/appcelerator/amp/data/elasticsearch"
 	"golang.org/x/net/context"
+	"gopkg.in/olivere/elastic.v3"
 )
 
 const (
@@ -17,24 +18,49 @@ type Logs struct {
 
 // Get implements log.LogServer
 func (s *Logs) Get(ctx context.Context, in *GetRequest) (*GetReply, error) {
-	reply := GetReply{}
-	// Search with a term query
-	//termQuery := elastic.NewTermQuery("user", "bquenin")
-	searchResult, err := s.ES.GetClient().Search().
+	// Prepare request to elasticsearch
+	request := s.ES.GetClient().Search().
 		Index(esIndex).
-		//Query(termQuery).
-		Sort("time_id", false).
-		Size(100).
-		Do()
-	if err != nil {
-		return &reply, err
+		Sort("timestamp", false)
+	// TODO timestamp queries
+	if in.From != 0 {
+		request.From(int(in.From))
 	}
+	if in.Size != 0 {
+		request.Size(int(in.Size))
+	} else {
+		request.Size(100)
+	}
+	if in.ServiceId != "" {
+		request.Query(elastic.NewTermQuery("service_id", in.ServiceId))
+	}
+	if in.ServiceName != "" {
+		request.Query(elastic.NewTermQuery("service_name", in.ServiceName))
+	}
+	if in.ContainerId != "" {
+		request.Query(elastic.NewTermQuery("container_id", in.ContainerId))
+	}
+	if in.NodeId != "" {
+		request.Query(elastic.NewTermQuery("node_id", in.NodeId))
+	}
+	if in.Message != "" {
+		request.Query(elastic.NewTermQuery("message", in.Message))
+	}
+
+	// Perform request
+	searchResult, err := request.Do()
+	if err != nil {
+		return nil, err
+	}
+
+	// Build reply (from elasticsearch response)
+	reply := GetReply{}
 	reply.Entries = make([]*LogEntry, len(searchResult.Hits.Hits))
 	for i, hit := range searchResult.Hits.Hits {
 		var entry LogEntry
 		err := json.Unmarshal(*hit.Source, &entry)
 		if err != nil {
-			return &reply, err
+			return nil, err
 		}
 		reply.Entries[i] = &entry
 	}
