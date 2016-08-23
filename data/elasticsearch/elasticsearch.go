8a61c6999518173edd3a1670ef961a21907c7377
bq <bquenin@axway.com>
2016-08-23 20:27:02 +0200

    Removing unused Elasticsearch code

--- data/elasticsearch/elasticsearch.go
+++ data/elasticsearch/elasticsearch.go
@@ -24,66 +24,6 @@ func (es *Elasticsearch) Connect(url string) error {
 	return err
 }
 
-// CreateIndexIfNotExists Creates an index if it doesn't already exists
-func (es *Elasticsearch) CreateIndexIfNotExists(esIndex string, esType string, mapping string) {
-	// Use the IndexExists service to check if the index exists
-	exists, err := client.IndexExists(esIndex).Do()
-	if err != nil {
-		// Handle error
-		panic(err)
-	}
-	if !exists {
-		// Create a new index.
-		createIndex, err := client.CreateIndex(esIndex).Do()
-		if err != nil {
-			// TODO: Handle error
-			panic(err)
-		}
-		if !createIndex.Acknowledged {
-			// TODO: Handle not acknowledged
-			panic("not acked")
-		}
-
-		response, err := client.PutMapping().Index(esIndex).Type(esType).BodyString(mapping).Do()
-		if err != nil {
-			// TODO: Handle error
-			panic(err)
-		}
-		if response == nil {
-			// TODO: Handle error
-			panic(err)
-		}
-	}
-}
-
-// Index store a document inside elastic search
-func (es *Elasticsearch) Index(esIndex string, esType string, body interface{}) {
-	// Add a document to the index
-	_, err := client.Index().
-		Index(esIndex).
-		Type("project").
-		BodyJson(body).
-		Refresh(true).
-		Do()
-	if err != nil {
-		// TODO: Handle error
-		panic(err)
-	}
-}
-
-// All returns all the documents for a given index
-func (es *Elasticsearch) All(esIndex string) []*elastic.SearchHit {
-	// Search with a term query
-	searchResult, err := client.Search().
-		Index(esIndex).
-		Do()
-	if err != nil {
-		// TODO: Handle error
-		panic(err)
-	}
-	return searchResult.Hits.Hits
-}
-
 // GetClient returns the native elastic search client
 func (es *Elasticsearch) GetClient() *elastic.Client {
 	return client
