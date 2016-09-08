5735ac71514c047ff5e7f3cebfd777e184cae65c
Chris Coy <chrisccoy@gmail.com>
2016-09-07 22:37:36 -0700

    Added Initial Support for etcd

--- api/rpc/project/project.pb.go
+++ api/rpc/project/project.pb.go
@@ -9,8 +9,11 @@ It is generated from these files:
 	project.proto
 
 It has these top-level messages:
-	CreateRequest
-	CreateReply
+	Project
+	Empty
+	ProjectRequest
+	ProjectReply
+	ProjectsReply
 */
 package project
 
@@ -34,28 +37,80 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type CreateRequest struct {
-	Id   string `protobuf:"bytes,1,opt,name=id" json:"id,omitempty"`
-	Name string `protobuf:"bytes,2,opt,name=name" json:"name,omitempty"`
+type Project struct {
+	RepoId    int32  `protobuf:"varint,1,opt,name=repo_id,json=repoId" json:"repo_id,omitempty"`
+	OwnerName string `protobuf:"bytes,2,opt,name=owner_name,json=ownerName" json:"owner_name,omitempty"`
+	RepoName  string `protobuf:"bytes,3,opt,name=repo_name,json=repoName" json:"repo_name,omitempty"`
+	Token     string `protobuf:"bytes,4,opt,name=token" json:"token,omitempty"`
 }
 
-func (m *CreateRequest) Reset()                    { *m = CreateRequest{} }
-func (m *CreateRequest) String() string            { return proto.CompactTextString(m) }
-func (*CreateRequest) ProtoMessage()               {}
-func (*CreateRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+func (m *Project) Reset()                    { *m = Project{} }
+func (m *Project) String() string            { return proto.CompactTextString(m) }
+func (*Project) ProtoMessage()               {}
+func (*Project) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
-type CreateReply struct {
-	Message string `protobuf:"bytes,1,opt,name=message" json:"message,omitempty"`
+type Empty struct {
 }
 
-func (m *CreateReply) Reset()                    { *m = CreateReply{} }
-func (m *CreateReply) String() string            { return proto.CompactTextString(m) }
-func (*CreateReply) ProtoMessage()               {}
-func (*CreateReply) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (m *Empty) Reset()                    { *m = Empty{} }
+func (m *Empty) String() string            { return proto.CompactTextString(m) }
+func (*Empty) ProtoMessage()               {}
+func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+
+type ProjectRequest struct {
+	Project *Project `protobuf:"bytes,1,opt,name=project" json:"project,omitempty"`
+}
+
+func (m *ProjectRequest) Reset()                    { *m = ProjectRequest{} }
+func (m *ProjectRequest) String() string            { return proto.CompactTextString(m) }
+func (*ProjectRequest) ProtoMessage()               {}
+func (*ProjectRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
+
+func (m *ProjectRequest) GetProject() *Project {
+	if m != nil {
+		return m.Project
+	}
+	return nil
+}
+
+type ProjectReply struct {
+	Project *Project `protobuf:"bytes,1,opt,name=project" json:"project,omitempty"`
+}
+
+func (m *ProjectReply) Reset()                    { *m = ProjectReply{} }
+func (m *ProjectReply) String() string            { return proto.CompactTextString(m) }
+func (*ProjectReply) ProtoMessage()               {}
+func (*ProjectReply) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{3} }
+
+func (m *ProjectReply) GetProject() *Project {
+	if m != nil {
+		return m.Project
+	}
+	return nil
+}
+
+type ProjectsReply struct {
+	Projects []*Project `protobuf:"bytes,1,rep,name=projects" json:"projects,omitempty"`
+}
+
+func (m *ProjectsReply) Reset()                    { *m = ProjectsReply{} }
+func (m *ProjectsReply) String() string            { return proto.CompactTextString(m) }
+func (*ProjectsReply) ProtoMessage()               {}
+func (*ProjectsReply) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
+
+func (m *ProjectsReply) GetProjects() []*Project {
+	if m != nil {
+		return m.Projects
+	}
+	return nil
+}
 
 func init() {
-	proto.RegisterType((*CreateRequest)(nil), "project.CreateRequest")
-	proto.RegisterType((*CreateReply)(nil), "project.CreateReply")
+	proto.RegisterType((*Project)(nil), "project.Project")
+	proto.RegisterType((*Empty)(nil), "project.Empty")
+	proto.RegisterType((*ProjectRequest)(nil), "project.ProjectRequest")
+	proto.RegisterType((*ProjectReply)(nil), "project.ProjectReply")
+	proto.RegisterType((*ProjectsReply)(nil), "project.ProjectsReply")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -66,64 +121,196 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion3
 
-// Client API for Project service
+// Client API for ProjectSvc service
 
-type ProjectClient interface {
-	Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateReply, error)
+type ProjectSvcClient interface {
+	Create(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error)
+	Delete(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error)
+	List(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*ProjectsReply, error)
+	Get(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error)
+	Update(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error)
 }
 
-type projectClient struct {
+type projectSvcClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewProjectClient(cc *grpc.ClientConn) ProjectClient {
-	return &projectClient{cc}
+func NewProjectSvcClient(cc *grpc.ClientConn) ProjectSvcClient {
+	return &projectSvcClient{cc}
 }
 
-func (c *projectClient) Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateReply, error) {
-	out := new(CreateReply)
-	err := grpc.Invoke(ctx, "/project.Project/Create", in, out, c.cc, opts...)
+func (c *projectSvcClient) Create(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error) {
+	out := new(ProjectReply)
+	err := grpc.Invoke(ctx, "/project.ProjectSvc/Create", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// Server API for Project service
-
-type ProjectServer interface {
-	Create(context.Context, *CreateRequest) (*CreateReply, error)
+func (c *projectSvcClient) Delete(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error) {
+	out := new(ProjectReply)
+	err := grpc.Invoke(ctx, "/project.ProjectSvc/Delete", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
-func RegisterProjectServer(s *grpc.Server, srv ProjectServer) {
-	s.RegisterService(&_Project_serviceDesc, srv)
+func (c *projectSvcClient) List(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*ProjectsReply, error) {
+	out := new(ProjectsReply)
+	err := grpc.Invoke(ctx, "/project.ProjectSvc/List", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
-func _Project_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CreateRequest)
+func (c *projectSvcClient) Get(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error) {
+	out := new(ProjectReply)
+	err := grpc.Invoke(ctx, "/project.ProjectSvc/Get", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *projectSvcClient) Update(ctx context.Context, in *ProjectRequest, opts ...grpc.CallOption) (*ProjectReply, error) {
+	out := new(ProjectReply)
+	err := grpc.Invoke(ctx, "/project.ProjectSvc/Update", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// Server API for ProjectSvc service
+
+type ProjectSvcServer interface {
+	Create(context.Context, *ProjectRequest) (*ProjectReply, error)
+	Delete(context.Context, *ProjectRequest) (*ProjectReply, error)
+	List(context.Context, *Empty) (*ProjectsReply, error)
+	Get(context.Context, *ProjectRequest) (*ProjectReply, error)
+	Update(context.Context, *ProjectRequest) (*ProjectReply, error)
+}
+
+func RegisterProjectSvcServer(s *grpc.Server, srv ProjectSvcServer) {
+	s.RegisterService(&_ProjectSvc_serviceDesc, srv)
+}
+
+func _ProjectSvc_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProjectRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ProjectServer).Create(ctx, in)
+		return srv.(ProjectSvcServer).Create(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/project.Project/Create",
+		FullMethod: "/project.ProjectSvc/Create",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ProjectServer).Create(ctx, req.(*CreateRequest))
+		return srv.(ProjectSvcServer).Create(ctx, req.(*ProjectRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Project_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "project.Project",
-	HandlerType: (*ProjectServer)(nil),
+func _ProjectSvc_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProjectRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProjectSvcServer).Delete(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/project.ProjectSvc/Delete",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProjectSvcServer).Delete(ctx, req.(*ProjectRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProjectSvc_List_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProjectSvcServer).List(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/project.ProjectSvc/List",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProjectSvcServer).List(ctx, req.(*Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProjectSvc_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProjectRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProjectSvcServer).Get(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/project.ProjectSvc/Get",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProjectSvcServer).Get(ctx, req.(*ProjectRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ProjectSvc_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ProjectRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProjectSvcServer).Update(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/project.ProjectSvc/Update",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProjectSvcServer).Update(ctx, req.(*ProjectRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _ProjectSvc_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "project.ProjectSvc",
+	HandlerType: (*ProjectSvcServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Create",
-			Handler:    _Project_Create_Handler,
+			Handler:    _ProjectSvc_Create_Handler,
+		},
+		{
+			MethodName: "Delete",
+			Handler:    _ProjectSvc_Delete_Handler,
+		},
+		{
+			MethodName: "List",
+			Handler:    _ProjectSvc_List_Handler,
+		},
+		{
+			MethodName: "Get",
+			Handler:    _ProjectSvc_Get_Handler,
+		},
+		{
+			MethodName: "Update",
+			Handler:    _ProjectSvc_Update_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -133,15 +320,23 @@ var _Project_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("project.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 148 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0x2d, 0x28, 0xca, 0xcf,
-	0x4a, 0x4d, 0x2e, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x87, 0x72, 0x95, 0x8c, 0xb9,
-	0x78, 0x9d, 0x8b, 0x52, 0x13, 0x4b, 0x52, 0x83, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x84, 0xf8,
-	0xb8, 0x98, 0x32, 0x53, 0x24, 0x18, 0x15, 0x18, 0x35, 0x38, 0x83, 0x98, 0x32, 0x53, 0x84, 0x84,
-	0xb8, 0x58, 0xf2, 0x12, 0x73, 0x53, 0x25, 0x98, 0xc0, 0x22, 0x60, 0xb6, 0x92, 0x3a, 0x17, 0x37,
-	0x4c, 0x53, 0x41, 0x4e, 0xa5, 0x90, 0x04, 0x17, 0x7b, 0x6e, 0x6a, 0x71, 0x71, 0x62, 0x7a, 0x2a,
-	0x54, 0x1f, 0x8c, 0x6b, 0xe4, 0xcc, 0xc5, 0x1e, 0x00, 0xb1, 0x48, 0xc8, 0x82, 0x8b, 0x0d, 0xa2,
-	0x47, 0x48, 0x4c, 0x0f, 0xe6, 0x16, 0x14, 0x9b, 0xa5, 0x44, 0x30, 0xc4, 0x0b, 0x72, 0x2a, 0x95,
-	0x18, 0x92, 0xd8, 0xc0, 0x4e, 0x36, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x1c, 0x74, 0x60, 0x63,
-	0xc3, 0x00, 0x00, 0x00,
+	// 283 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x9c, 0x92, 0x41, 0x4b, 0xc3, 0x40,
+	0x10, 0x85, 0x4d, 0xd3, 0x24, 0xcd, 0x68, 0x8b, 0x2c, 0x6a, 0x43, 0x45, 0x08, 0x39, 0x15, 0x91,
+	0x22, 0xf5, 0x20, 0x14, 0x3d, 0xa9, 0x88, 0x20, 0x22, 0x11, 0xcf, 0x25, 0x36, 0x73, 0xa8, 0x26,
+	0xd9, 0x75, 0x77, 0x55, 0xf2, 0x9b, 0xfc, 0x93, 0x92, 0xc9, 0x26, 0x07, 0xeb, 0xc5, 0xdc, 0x76,
+	0xde, 0x7b, 0x1f, 0xfb, 0x66, 0x59, 0x18, 0x0a, 0xc9, 0x5f, 0x71, 0xa5, 0x67, 0x42, 0x72, 0xcd,
+	0x99, 0x67, 0xc6, 0x48, 0x83, 0xf7, 0x58, 0x1f, 0xd9, 0x18, 0x3c, 0x89, 0x82, 0x2f, 0xd7, 0x69,
+	0x60, 0x85, 0xd6, 0xd4, 0x89, 0xdd, 0x6a, 0xbc, 0x4b, 0xd9, 0x11, 0x00, 0xff, 0x2a, 0x50, 0x2e,
+	0x8b, 0x24, 0xc7, 0xa0, 0x17, 0x5a, 0x53, 0x3f, 0xf6, 0x49, 0x79, 0x48, 0x72, 0x64, 0x87, 0xe0,
+	0x13, 0x47, 0xae, 0x4d, 0xee, 0xa0, 0x12, 0xc8, 0xdc, 0x03, 0x47, 0xf3, 0x37, 0x2c, 0x82, 0x3e,
+	0x19, 0xf5, 0x10, 0x79, 0xe0, 0xdc, 0xe4, 0x42, 0x97, 0xd1, 0x05, 0x8c, 0xcc, 0xf5, 0x31, 0xbe,
+	0x7f, 0xa0, 0xd2, 0xec, 0x18, 0x9a, 0x6e, 0xd4, 0x62, 0x7b, 0xbe, 0x3b, 0x6b, 0xaa, 0x37, 0xc9,
+	0xb6, 0xfc, 0x02, 0x76, 0x5a, 0x5a, 0x64, 0xe5, 0xbf, 0xd8, 0x4b, 0x18, 0x1a, 0x4d, 0xd5, 0xf0,
+	0x09, 0x0c, 0x8c, 0xa7, 0x02, 0x2b, 0xb4, 0xff, 0xa4, 0xdb, 0xc4, 0xfc, 0xbb, 0x07, 0x60, 0xd4,
+	0xa7, 0xcf, 0x15, 0x5b, 0x80, 0x7b, 0x25, 0x31, 0xd1, 0xc8, 0xc6, 0x1b, 0x50, 0xbd, 0xd8, 0x64,
+	0x7f, 0xd3, 0x10, 0x59, 0x19, 0x6d, 0x55, 0xec, 0x35, 0x66, 0xd8, 0x89, 0x3d, 0x85, 0xfe, 0xfd,
+	0x5a, 0x69, 0x36, 0x6a, 0x03, 0xf4, 0xae, 0x93, 0x83, 0xdf, 0x80, 0x6a, 0x88, 0x73, 0xb0, 0x6f,
+	0x51, 0x77, 0xab, 0xf9, 0x2c, 0xd2, 0x4e, 0x2b, 0xbe, 0xb8, 0xf4, 0xeb, 0xce, 0x7e, 0x02, 0x00,
+	0x00, 0xff, 0xff, 0x1e, 0xe3, 0xf8, 0xfa, 0x86, 0x02, 0x00, 0x00,
 }
--- api/rpc/project/project_test.go
+++ api/rpc/project/project_test.go
@@ -1,30 +1,139 @@
 package project
 
-// import (
-// 	"github.com/satori/go.uuid"
-// 	"golang.org/x/net/context"
-// 	"google.golang.org/grpc"
-// 	"testing"
-// )
-//
-// const (
-// 	address = "localhost:50101"
-// )
-//
-// func TestShouldSucceedWhenProvidingAValidCreateRequest(t *testing.T) {
-// 	// Set up a connection to the server.
-// 	conn, err := grpc.Dial(address, grpc.WithInsecure())
-// 	if err != nil {
-// 		t.Fatalf("did not connect: %v", err)
-// 	}
-//
-// 	// Contact the server and print out its response.
-// 	c := project.NewProjectClient(conn)
-// 	r, err := c.Create(context.Background(), &project.CreateRequest{Name: "test-project-" + uuid.NewV4().String()})
-// 	if err != nil {
-// 		t.Fatalf("could not greet: %v", err)
-// 	}
-// 	t.Logf("Greeting: %s", r.Message)
-//
-// 	conn.Close()
-// }
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/appcelerator/amp/data/storage/etcd"
+	"github.com/golang/protobuf/proto"
+)
+
+const (
+	etcdDefaultEndpoint = "http://localhost:2379"
+)
+
+var (
+	etcdEndpoints    = []string{etcdDefaultEndpoint}
+	proj             *Proj
+	sampleProject    = &ProjectRequest{&Project{RepoId: 12345, OwnerName: "amp", RepoName: "amp-repo", Token: "FakeToken"}}
+	updSampleProject = &ProjectRequest{&Project{RepoId: 12345, OwnerName: "amp", RepoName: "amp-repo2", Token: "FakeToken"}}
+	sampleProject2   = &ProjectRequest{&Project{RepoId: 12346, OwnerName: "amp", RepoName: "amp-repo", Token: "FakeToken"}}
+)
+
+func TestMain(m *testing.M) {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Lshortfile)
+	log.SetPrefix("test: ")
+	proj = createProjectServer()
+	os.Exit(m.Run())
+}
+
+func TestCreate(t *testing.T) {
+	proj.Delete(context.Background(), sampleProject)
+	resp, err := proj.Create(context.Background(), sampleProject)
+	if err != nil {
+		t.Error(err)
+	}
+	resp, err = proj.Get(context.Background(), sampleProject)
+	if err != nil {
+		t.Error(err)
+	}
+	if !proto.Equal(resp.Project, sampleProject.Project) {
+		t.Errorf("expected %v, got %v", sampleProject.Project, resp.Project)
+	}
+}
+
+func TestCreateAlreadyExists(t *testing.T) {
+	// Guarantee no data exists
+	proj.Delete(context.Background(), sampleProject)
+	// Create new Entry
+	proj.Create(context.Background(), sampleProject)
+	// Attempt to create a duplicate
+	_, err := proj.Create(context.Background(), sampleProject)
+	// Should result in a duplicate entry
+	if !strings.Contains(err.Error(), "key already exists") {
+		t.Error("Duplicate Key Error was not detected")
+	}
+}
+
+func TestGet(t *testing.T) {
+	// Guarantee no data exists
+	proj.Delete(context.Background(), sampleProject)
+	// Create new Entry
+	proj.Create(context.Background(), sampleProject)
+	// Fetch The Entry
+
+	resp, err := proj.Get(context.Background(), sampleProject)
+	if err != nil {
+		t.Error(err)
+	}
+	if !proto.Equal(resp.Project, sampleProject.Project) {
+		t.Errorf("expected %v, got %v", sampleProject.Project, resp.Project)
+	}
+}
+func TestUpdate(t *testing.T) {
+	// Guarantee no data exists
+	proj.Delete(context.Background(), sampleProject)
+	// Create new Entry
+	proj.Create(context.Background(), sampleProject)
+	// Update The Entry
+	resp, err := proj.Update(context.Background(), updSampleProject)
+	if err != nil {
+		t.Error(err)
+	}
+	resp, _ = proj.Get(context.Background(), sampleProject)
+	if !proto.Equal(resp.Project, updSampleProject.Project) {
+		t.Errorf("expected %v, got %v", updSampleProject.Project, resp.Project)
+	}
+}
+
+func TestList(t *testing.T) {
+	// Guarantee no data exists
+	proj.Delete(context.Background(), sampleProject)
+	proj.Delete(context.Background(), sampleProject2)
+	// Create new Entries
+	proj.Create(context.Background(), sampleProject)
+	proj.Create(context.Background(), sampleProject2)
+	// Fetch The Entry
+	resp, err := proj.List(context.Background(), &Empty{})
+	if err != nil {
+		t.Error(err)
+	} else if len(resp.Projects) != 2 {
+		t.Errorf("Expected length of 2, got %v\n", len(resp.Projects))
+	}
+
+}
+
+func TestDelete(t *testing.T) {
+	// Guarantee no data exists
+	proj.Delete(context.Background(), sampleProject)
+	// Create new Entry
+	proj.Create(context.Background(), sampleProject)
+	// Delete The Entry
+	resp, err := proj.Delete(context.Background(), sampleProject)
+	if err != nil {
+		t.Error(err)
+	}
+	if !proto.Equal(resp.Project, sampleProject.Project) {
+		t.Errorf("expected %v, got %v", sampleProject.Project, resp.Project)
+	}
+}
+
+// Create the ProjectServer as a local struct that can be excercised directly over the call stack
+func createProjectServer() *Proj {
+	//Create the config
+	var proj = &Proj{}
+
+	if endpoints := os.Getenv("endpoints"); endpoints != "" {
+		etcdEndpoints = strings.Split(endpoints, ",")
+	}
+	proj.Store = etcd.New(etcdEndpoints, "amp")
+	if err := proj.Store.Connect(5 * time.Second); err != nil {
+		panic(err)
+	}
+	return proj
+}
