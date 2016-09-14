bc774a239cbb58539b91fc954a9934ada2ef5c82
Francois <freignat91@users.noreply.github.com>
2016-09-14 03:43:06 +0200

    add registry to amp with HAProxy reverse proxy (#155)
    * add registry
    
    * update haproxy
    
    * update image list
    
    * update registry volume
    
    * add amp command: push
    
    * add persistence to registry
    
    * minor format update

---
+++ cmd/amp/registry.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/appcelerator/amp/api/client"
+	"github.com/spf13/cobra"
+)
+
+var pushCmd = &cobra.Command{
+	Use:   "push",
+	Short: "push an image in the amp registry",
+	Long:  `push an image to the amp registry`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := RegistryPush(AMP, cmd, args)
+		if err != nil {
+			fmt.Println(err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(pushCmd)
+}
+
+// RegistryPush displays resource usage statistcs
+func RegistryPush(amp *client.AMP, cmd *cobra.Command, args []string) error {
+	_, err := amp.GetAuthorizedContext()
+	if err != nil {
+		return err
+	}
+
+	image := args[0]
+
+	if amp.Verbose() {
+		fmt.Println("Execute registry push command with:")
+		fmt.Printf("image: %s\n", image)
+	}
+
+	if err = validateRegistryImage(image); err != nil {
+		return err
+	}
+	cmdexe := exec.Command("docker", "push", image)
+	cmdexe.Stdout = os.Stdout
+	cmdexe.Stderr = os.Stderr
+	err = cmdexe.Run()
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func validateRegistryImage(image string) error {
+	if image == "" {
+		return errors.New("need a valid image name")
+	}
+	return nil
+}
