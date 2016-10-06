ad15ac8611a7b418ef8ae25f37c4d531f5433294
Francois <freignat91@users.noreply.github.com>
2016-10-06 16:17:14 +0200

    add registry ls and auto tag (#292)
    * add registry ls and auto tag

--- cmd/amp/registry.go
+++ cmd/amp/registry.go
@@ -5,28 +5,57 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
+	"net/http"
+	"io/ioutil"
 
 	"github.com/appcelerator/amp/api/client"
 	"github.com/spf13/cobra"
 )
 
-var pushCmd = &cobra.Command{
-	Use:   "push",
-	Short: "Push an image in the amp registry",
-	Long:  `Push an image to the amp registry`,
-	Run: func(cmd *cobra.Command, args []string) {
-		err := RegistryPush(AMP, cmd, args)
-		if err != nil {
-			fmt.Println(err)
-		}
-	},
+// Registry is the main command for attaching registry subcommands.
+var RegCmd = &cobra.Command{
+	Use:   "registry operations",
+	Short: "Registry operations",
+	Long:  `Manage registry-related operations.`,
 }
+
+var (
+	domain = "local.appcelerator.io"
+	pushCmd = &cobra.Command{
+		Use:   "push [image]",
+		Short: "Push an image to the amp registry",
+		Long:  `Push an image to the amp registry`,
+		Run: func(cmd *cobra.Command, args []string) {
+			err := RegistryPush(AMP, cmd, args)
+			if err != nil {
+				fmt.Println(err)
+			}
+		},
+	}
+	reglsCmd = &cobra.Command{
+		Use:   "ls",
+		Short: "List the amp registry images",
+		Long:  `List the amp registry images`,
+		Run: func(cmd *cobra.Command, args []string) {
+			err := RegistryLs(AMP, cmd, args)
+			if err != nil {
+				fmt.Println(err)
+			}
+		},
+	}	
+)
+
+
 
 func init() {
-	RootCmd.AddCommand(pushCmd)
+	RootCmd.AddCommand(RegCmd)
+	pushCmd.Flags().StringVar(&domain, "domain", domain, "The amp domain")
+	RegCmd.AddCommand(pushCmd)
+	RegCmd.AddCommand(reglsCmd)
 }
 
-// RegistryPush displays resource usage statistcs
+// RegistryPush displays resource usage statistics
 func RegistryPush(amp *client.AMP, cmd *cobra.Command, args []string) error {
 	_, err := amp.GetAuthorizedContext()
 	if err != nil {
@@ -43,13 +72,46 @@ func RegistryPush(amp *client.AMP, cmd *cobra.Command, args []string) error {
 	if err = validateRegistryImage(image); err != nil {
 		return err
 	}
-	cmdexe := exec.Command("docker", "push", image)
+	taggedImage := image
+	if !strings.HasPrefix(image, "registry."+domain) {
+		nn := strings.Index(image, "/")
+		if (nn < 0) {
+			return fmt.Errorf("Invalid image name %s", image)
+		}
+		taggedImage = "registry."+domain+"/"+image[nn+1:]
+		fmt.Printf("Tag image from %s to %s\n", image, taggedImage)
+		cmdexe := exec.Command("docker", "tag", image, taggedImage)
+		cmdexe.Stdout = os.Stdout
+		cmdexe.Stderr = os.Stderr
+		err = cmdexe.Run()
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Printf("push image %s\n", taggedImage)
+	cmdexe := exec.Command("docker", "push", taggedImage)
 	cmdexe.Stdout = os.Stdout
 	cmdexe.Stderr = os.Stderr
 	err = cmdexe.Run()
 	if err != nil {
 		return err
 	}
+	return err
+}
+
+// RegistryPush displays resource usage statistics: localhost:5000/v2/_catalog
+func RegistryLs(amp *client.AMP, cmd *cobra.Command, args []string) error {
+	_, err := amp.GetAuthorizedContext()
+	if err != nil {
+		return err
+	}
+	resp, err := http.Get("http://registry."+domain+"/v2/_catalog")
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 	return err
 }
 
