55eafbacacb1156b9f996d0ffd91284632a32f49
qube <bquenin@axway.com>
2016-10-18 17:03:18 +0200

    Fixing log command (#338)

--- api/server/server_utils.go
+++ api/server/server_utils.go
@@ -88,24 +88,14 @@ func StartTestServer() (Config, *grpc.ClientConn) {
 
 	// Connect to amplifier
 	log.Println("Connecting to amplifier")
-	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure(), grpc.WithTimeout(60*time.Second))
+	conn, err := grpc.Dial(serverAddress,
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+		grpc.WithTimeout(60*time.Second))
 	if err != nil {
-		log.Panicln("Amplifier is not ready", err)
+		log.Panicln("Cannot connect to amplifier", err)
 	}
 	log.Println("Connected to amplifier")
-
-	// Use the connection to amplifier
-	logsClient := logs.NewLogsClient(conn)
-	for i := 0; i < 60; i++ {
-		_, err := logsClient.Get(context.Background(), &logs.GetRequest{})
-		if err != nil {
-			log.Println("Connection to amplifier not ready yet: ", err)
-			time.Sleep(100 * time.Millisecond)
-			continue
-		}
-		break
-	}
-	log.Println("Ready")
 
 	return config, conn
 }
--- cmd/amp/logs.go
+++ cmd/amp/logs.go
@@ -15,9 +15,13 @@ var logsCmd = &cobra.Command{
 	Use:   "logs [OPTIONS] [SERVICE]",
 	Short: "Fetch log entries matching provided criteria. If provided, SERVICE can be a partial or full service id or service name.",
 	Run: func(cmd *cobra.Command, args []string) {
+		AMP.Connect()
 		err := Logs(AMP, cmd, args)
 		if err != nil {
-			fmt.Println(err)
+			if AMP.Verbose() {
+				log.Println(err)
+			}
+			log.Fatal("Failed to get logs")
 		}
 	},
 }
