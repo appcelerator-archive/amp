d0a6529d143cee1591490dcda97f4a3e5c027102
freignat <freignat@axway.com>
2016-11-09 10:09:47 +0100

    add amp commands

---
+++ cmd/amp/platform_pull.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// PlatformPull is the main command for attaching platform subcommands.
+var PlatformPull = &cobra.Command{
+	Use:   "pull",
+	Short: "Pull platform images",
+	Long:  `Pull all AMP platform images`,
+	Run: func(cmd *cobra.Command, args []string) {
+		pullAMPImages(cmd, args)
+	},
+}
+
+func init() {
+	PlatformPull.Flags().BoolP("silence", "s", false, "no console output at all")
+	PlatformCmd.AddCommand(PlatformPull)
+}
+
+func pullAMPImages(cmd *cobra.Command, args []string) error {
+	manager := &ampManager{}
+	if cmd.Flag("silence").Value.String() == "true" {
+		manager.silence = true
+	}
+	if cmd.Flag("verbose").Value.String() == "true" {
+		manager.verbose = true
+	}
+	if err := manager.init("Pulling AMP images"); err != nil {
+		manager.printf(colError, "Pull error: %v\n", err)
+		os.Exit(1)
+	}
+	if cmd.Flag("server").Value.String() != "" {
+		manager.printf(colWarn, "Error: --server has no effect for pull command\n")
+		os.Exit(1)
+	}
+	if err := manager.pull(getAMPInfrastructureStack(manager)); err != nil {
+		manager.printf(colError, "Pull error: %v\n", err)
+		os.Exit(1)
+	}
+	manager.printf(colWhite, "AMP platform images pulled\n")
+	return nil
+}
---
+++ cmd/amp/platform_stop.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// PlatformStop is the main command for attaching platform subcommands.
+var PlatformStop = &cobra.Command{
+	Use:   "stop",
+	Short: "Stop platform",
+	Long:  `Stop all AMP platform services.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		stopAMP(cmd, args)
+	},
+}
+
+func init() {
+	PlatformStop.Flags().BoolP("silence", "s", false, "no console output at all")
+	PlatformCmd.AddCommand(PlatformStop)
+}
+
+func stopAMP(cmd *cobra.Command, args []string) error {
+	manager := &ampManager{}
+	if cmd.Flag("silence").Value.String() == "true" {
+		manager.silence = true
+	}
+	if cmd.Flag("verbose").Value.String() == "true" {
+		manager.verbose = true
+	}
+	if err := manager.init("Stopping AMP platform"); err != nil {
+		manager.printf(colError, "Start error: %v\n", err)
+		os.Exit(1)
+	}
+	if cmd.Flag("server").Value.String() != "" {
+		manager.printf(colWarn, "Error: --server has no effect for stop command\n")
+		os.Exit(1)
+	}
+	stack := getAMPInfrastructureStack(manager)
+	manager.computeStatus(stack)
+	if manager.status == "stopped" {
+		manager.printf(colWhite, "AMP platform already stopped\n")
+		return nil
+	}
+	if err := manager.stop(stack); err != nil {
+		manager.printf(colError, "Stop error: %v\n", err)
+		os.Exit(1)
+	}
+	manager.printf(colWhite, "AMP platform stopped\n")
+	return nil
+}
