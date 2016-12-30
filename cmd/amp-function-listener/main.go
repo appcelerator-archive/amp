1e0bc0142d65b0351dd7b1b595aca92be3f9aaa6
Henry Allen-Tilford <htilford@gmail.com>
2016-12-30 11:10:26 -0800

    Add functions to the ui (#605)

--- cmd/amp-function-listener/main.go
+++ cmd/amp-function-listener/main.go
@@ -60,6 +60,27 @@ const (
 	listenAddr = ":80"
 )
 
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	return
+}
+
 func main() {
 	log.Printf("%s (version: %s, build: %s)\n", os.Args[0], Version, Build)
 
@@ -95,7 +116,7 @@ func main() {
 	router.POST("/:function", Index)
 
 	log.Println("Start listening on", listenAddr)
-	log.Fatal(http.ListenAndServe(listenAddr, router))
+	log.Fatal(http.ListenAndServe(listenAddr, allowCORS(router)))
 }
 
 // Index index
