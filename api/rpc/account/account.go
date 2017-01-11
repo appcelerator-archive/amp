a90961947190e9d52e66d1c163fb8f75c3312f6b
Henry Allen-Tilford <htilford@gmail.com>
2017-01-10 16:18:28 -0800

    Add account stubs
    Add missing dep
    
    Empty reference, partial validators
    
    fix

---
+++ api/rpc/account/account.go
@@ -0,0 +1,357 @@
+package account
+
+import (
+	"fmt"
+	"net/mail"
+
+	context "golang.org/x/net/context"
+
+	pb "github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"gopkg.in/hlandau/passlib.v1"
+)
+
+const codeLength = 8
+const hash = "$s2$16384$8$1$42JtddBgSqrJMwc3YuTNW+R+$ISfEF3jkvYQYk4AK/UFAxdqnmNFVeUw2gUVXEMBDAng=" // password
+
+// Validate validates SignUpRequest
+func (r *SignUpRequest) Validate() (err error) {
+	if r.Name == "" {
+		return grpc.Errorf(codes.InvalidArgument, "user name is mandatory")
+	}
+	address, err := mail.ParseAddress(r.Email)
+	if err != nil {
+		return grpc.Errorf(codes.InvalidArgument, err.Error())
+	}
+	r.Email = address.Address
+	if len(r.Password) < 8 {
+		return grpc.Errorf(codes.InvalidArgument, "password too weak")
+	}
+	return
+}
+
+// Validate validates VerificationRequest
+func (r *VerificationRequest) Validate() (err error) {
+	if len(r.Code) != codeLength {
+		return grpc.Errorf(codes.InvalidArgument, "invalid verification code")
+	}
+	return
+}
+
+// Validate validates OrganizationRequest
+func (r *OrganizationRequest) Validate() (err error) {
+	if r.Name == "" {
+		return grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	address, err := mail.ParseAddress(r.Email)
+	if err != nil {
+		return grpc.Errorf(codes.InvalidArgument, err.Error())
+	}
+	r.Email = address.Address
+	return
+}
+
+// Server is used to implement account.AccountServer
+type Server struct{}
+
+// SignUp implements account.SignUp
+func (s *Server) SignUp(ctx context.Context, in *SignUpRequest) (out *SessionReply, err error) {
+	out = &SessionReply{}
+	err = in.Validate()
+	if err != nil {
+		return nil, err
+	}
+	_, err = passlib.Hash(in.Password)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, "hashing error")
+	}
+	out.SessionKey = in.Name
+	return
+}
+
+// Verify implements account.Verify
+func (s *Server) Verify(ctx context.Context, in *VerificationRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	err = in.Validate()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(in.Code)
+	return
+}
+
+// CreateOrganization implements account.CreateOranization
+func (s *Server) CreateOrganization(ctx context.Context, in *OrganizationRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	err = in.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// Login implements account.Login
+func (s *Server) Login(ctx context.Context, in *LogInRequest) (out *SessionReply, err error) {
+	out = &SessionReply{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	_, err = passlib.Verify(in.Password, hash)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Unauthenticated, err.Error())
+	}
+	out.SessionKey = in.Name
+	return
+
+}
+
+// Switch implements account.Switch
+func (s *Server) Switch(ctx context.Context, in *TeamRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// ListAccounts implements account.ListAccounts
+func (s *Server) ListAccounts(ctx context.Context, in *AccountsRequest) (out *ListReply, err error) {
+	out = &ListReply{}
+	if in.Type != "individual" && in.Type != "organization" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "account type is mandatory")
+	}
+	return
+}
+
+// GetAccountDetails implements account.GetAccountDetails
+func (s *Server) GetAccountDetails(ctx context.Context, in *AccountRequest) (out *AccountReply, err error) {
+	out = &AccountReply{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "name is mandatory")
+	}
+	return
+}
+
+// EditAccount implements account.EditAccount
+func (s *Server) EditAccount(ctx context.Context, in *EditRequest) (out *AccountReply, err error) {
+	out = &AccountReply{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "name is mandatory")
+	}
+	if in.Email != "" {
+		_, err := mail.ParseAddress(in.Email)
+		if err != nil {
+			return nil, grpc.Errorf(codes.InvalidArgument, err.Error())
+		}
+	}
+	if in.NewPassword != "" {
+		_, err := passlib.Verify(in.Password, hash)
+		if err != nil {
+			return nil, grpc.Errorf(codes.Unauthenticated, err.Error())
+		}
+		if len(in.NewPassword) < 8 {
+			return nil, grpc.Errorf(codes.InvalidArgument, "password too weak")
+		}
+		_, err = passlib.Hash(in.NewPassword)
+		if err != nil {
+			return nil, grpc.Errorf(codes.Internal, "hashing error")
+		}
+	}
+	return
+}
+
+// DeleteAccount implements account.DeleteAccount
+func (s *Server) DeleteAccount(ctx context.Context, in *AccountRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "name is mandatory")
+	}
+	return
+}
+
+// AddOrganizationMemberships implements account.AddOrganizationMemberships
+func (s *Server) AddOrganizationMemberships(ctx context.Context, in *OrganizationMembershipsRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if len(in.Members) == 0 {
+		return nil, grpc.Errorf(codes.InvalidArgument, "members are mandatory")
+	}
+	return
+}
+
+// DeleteOrganizationMemberships implements account.DeleteOrganizationMemberships
+func (s *Server) DeleteOrganizationMemberships(ctx context.Context, in *OrganizationMembershipsRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if len(in.Members) == 0 {
+		return nil, grpc.Errorf(codes.InvalidArgument, "members are mandatory")
+	}
+	return
+}
+
+// CreateTeam implements account.CreateTeam
+func (s *Server) CreateTeam(ctx context.Context, in *TeamRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// ListTeams implements account.ListTeams
+func (s *Server) ListTeams(ctx context.Context, in *TeamRequest) (out *ListReply, err error) {
+	out = &ListReply{}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// EditTeam implements account.EditTeam
+func (s *Server) EditTeam(ctx context.Context, in *TeamRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// GetTeamDetails implements account.GetTeamDetails
+func (s *Server) GetTeamDetails(ctx context.Context, in *TeamRequest) (out *TeamReply, err error) {
+	out = &TeamReply{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// DeleteTeam implements account.DeleteTeam
+func (s *Server) DeleteTeam(ctx context.Context, in *TeamRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// AddTeamMemberships implements account.AddTeamMemberships
+func (s *Server) AddTeamMemberships(ctx context.Context, in *TeamMembershipsRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if len(in.Members) == 0 {
+		return nil, grpc.Errorf(codes.InvalidArgument, "members are mandatory")
+	}
+	return
+}
+
+// DeleteTeamMemberships implements account.DeleteTeamMemberships
+func (s *Server) DeleteTeamMemberships(ctx context.Context, in *TeamMembershipsRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Name == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if len(in.Members) == 0 {
+		return nil, grpc.Errorf(codes.InvalidArgument, "members are mandatory")
+	}
+	return
+}
+
+// GrantPermission implements account.GrantPermission
+func (s *Server) GrantPermission(ctx context.Context, in *PermissionRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Team == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if in.Level == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "permission level is mandatory")
+	}
+	if in.ResourceId == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "resource id is mandatory")
+	}
+	return
+}
+
+// ListPermissions implements account.ListPermissions
+func (s *Server) ListPermissions(ctx context.Context, in *PermissionRequest) (out *ListReply, err error) {
+	out = &ListReply{}
+	if in.Team != "" && in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	return
+}
+
+// EditPermission implements account.EditPermission
+func (s *Server) EditPermission(ctx context.Context, in *PermissionRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Team == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if in.Level == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "permission level is mandatory")
+	}
+	if in.ResourceId == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "resource id is mandatory")
+	}
+	return
+}
+
+// RevokePermission implements account.RevokePermission
+func (s *Server) RevokePermission(ctx context.Context, in *PermissionRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Team == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if in.ResourceId == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "resource id is mandatory")
+	}
+	return
+}
+
+// TransferOwnership implements account.TransferOwnership
+func (s *Server) TransferOwnership(ctx context.Context, in *PermissionRequest) (out *pb.Empty, err error) {
+	out = &pb.Empty{}
+	if in.Team == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "team name is mandatory")
+	}
+	if in.Organization == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "organization name is mandatory")
+	}
+	if in.ResourceId == "" {
+		return nil, grpc.Errorf(codes.InvalidArgument, "resource id is mandatory")
+	}
+	return
+}
