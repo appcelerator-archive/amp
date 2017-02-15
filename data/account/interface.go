9248617bc1f56495082150f1ec73e735169312b6
qube <bquenin@axway.com>
2017-02-15 10:42:31 +0100

    Adding user management backend and tests

---
+++ data/account/interface.go
@@ -0,0 +1,31 @@
+package account
+
+import (
+	"context"
+
+	"github.com/appcelerator/amp/data/account/schema"
+)
+
+// Interface defines the user data access layer
+type Interface interface {
+	// CreateUser creates a new user
+	CreateUser(ctx context.Context, user *schema.User) (id string, err error)
+
+	// GetUser fetches an user by id
+	GetUser(ctx context.Context, id string) (user *schema.User, err error)
+
+	// GetUserByName fetches an user by name
+	GetUserByName(ctx context.Context, name string) (user *schema.User, err error)
+
+	// ListUsers lists users
+	ListUsers(ctx context.Context) (users []*schema.User, err error)
+
+	// UpdateUser updates an user
+	UpdateUser(ctx context.Context, update *schema.User) (err error)
+
+	// DeleteUser deletes an user by id
+	DeleteUser(ctx context.Context, id string) (err error)
+
+	// Reset resets the user store
+	Reset(ctx context.Context) (err error)
+}
--- tests/integration/rpc/main_test.go
+++ tests/integration/rpc/main_test.go
@@ -1,40 +1,41 @@
 package tests
 
 import (
-	"log"
-	"os"
-	"testing"
-	//"time"
-
-	"golang.org/x/net/context"
-	//"google.golang.org/grpc"
-
-	//tested packages
+	"github.com/appcelerator/amp/api/rpc/account"
 	"github.com/appcelerator/amp/api/rpc/function"
 	"github.com/appcelerator/amp/api/rpc/logs"
 	"github.com/appcelerator/amp/api/rpc/service"
 	"github.com/appcelerator/amp/api/rpc/stack"
 	"github.com/appcelerator/amp/api/rpc/stats"
 	"github.com/appcelerator/amp/api/rpc/topic"
+	as "github.com/appcelerator/amp/data/account"
 	"github.com/appcelerator/amp/data/storage"
 	"github.com/appcelerator/amp/data/storage/etcd"
 	"github.com/appcelerator/amp/pkg/config"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"log"
+	"os"
+	"testing"
 	"time"
 )
 
 var (
 	ctx            context.Context
+	store          storage.Interface
 	functionClient function.FunctionClient
 	statsClient    stats.StatsClient
 	stackClient    stack.StackServiceClient
 	topicClient    topic.TopicClient
 	serviceClient  service.ServiceClient
 	logsClient     logs.LogsClient
-	store          storage.Interface
+	accountClient  account.AccountClient
+	accountStore   as.Interface
 )
 
 func TestMain(m *testing.M) {
+	ctx = context.Background()
+
 	// Connect to amplifier
 	log.Println("Connecting to amplifier")
 	conn, err := grpc.Dial(amp.AmplifierDefaultEndpoint,
@@ -46,21 +47,22 @@ func TestMain(m *testing.M) {
 	}
 	log.Println("Connected to amplifier")
 
+	// Stores
 	store = etcd.New([]string{amp.EtcdDefaultEndpoint}, "amp")
 	if err := store.Connect(5 * time.Second); err != nil {
 		log.Panicf("Unable to connect to etcd on: %s\n%v", amp.EtcdDefaultEndpoint, err)
 	}
+	accountStore = as.NewStore(store)
 
-	ctx = context.Background()
-
-	// init package clients
+	// Clients init
 	functionClient = function.NewFunctionClient(conn)
 	statsClient = stats.NewStatsClient(conn)
 	stackClient = stack.NewStackServiceClient(conn)
 	topicClient = topic.NewTopicClient(conn)
 	serviceClient = service.NewServiceClient(conn)
 	logsClient = logs.NewLogsClient(conn)
+	accountClient = account.NewAccountClient(conn)
 
-	// start tests
+	// Start tests
 	os.Exit(m.Run())
 }
