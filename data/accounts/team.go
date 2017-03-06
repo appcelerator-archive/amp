da2735da1a07a7014682110087ed709c245d7cbd
qube <bquenin@axway.com>
2017-03-06 16:29:28 +0100

    working on function permissions

--- data/accounts/team.go
+++ data/accounts/team.go
@@ -1,14 +1,5 @@
 package accounts
 
-func (t *Team) getOwners() (owners []*TeamMember) {
-	for _, member := range t.Members {
-		if member.Role == TeamRole_TEAM_OWNER {
-			owners = append(owners, member)
-		}
-	}
-	return
-}
-
 func (t *Team) getMemberIndex(memberName string) int {
 	memberIndex := -1
 	for i, member := range t.Members {
