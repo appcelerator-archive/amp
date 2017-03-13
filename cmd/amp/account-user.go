8c8ca48d603294fcb7992ccbed3ea796fe6dd516
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-03-13 14:22:13 -0700

    Modified password command

--- cmd/amp/account-user.go
+++ cmd/amp/account-user.go
@@ -43,41 +43,6 @@ var (
 		},
 	}
 
-	pwdCmd = &cobra.Command{
-		Use:   "password",
-		Short: "Account password operations",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return pwd()
-		},
-	}
-
-	pwdChangeCmd = &cobra.Command{
-		Use:     "change",
-		Short:   "Change account password",
-		Example: "amp user password change --name=jdoe --password=p@s5wrd --new-password=v@larm0rghuli$",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return pwdChange(AMP, cmd)
-		},
-	}
-
-	pwdResetCmd = &cobra.Command{
-		Use:     "reset",
-		Short:   "Reset account password",
-		Example: "amp user password reset --name=jdoe",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return pwdReset(AMP, cmd)
-		},
-	}
-
-	pwdSetCmd = &cobra.Command{
-		Use:     "set",
-		Short:   "Set account password",
-		Example: "amp user password set --token=this-is-a-token-sample --password=v@lard0haeri$",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return pwdSet(AMP, cmd)
-		},
-	}
-
 	listUserCmd = &cobra.Command{
 		Use:     "ls",
 		Short:   "List user",
@@ -106,19 +71,14 @@ var (
 		},
 	}
 
-	email  string
-	token  string
-	newPwd string
+	email string
+	token string
 )
 
 func init() {
 	UserCmd.AddCommand(signUpCmd)
 	UserCmd.AddCommand(verifyCmd)
 	UserCmd.AddCommand(forgotLoginCmd)
-	UserCmd.AddCommand(pwdCmd)
-	pwdCmd.AddCommand(pwdChangeCmd)
-	pwdCmd.AddCommand(pwdResetCmd)
-	pwdCmd.AddCommand(pwdSetCmd)
 	UserCmd.AddCommand(listUserCmd)
 	UserCmd.AddCommand(deleteUserCmd)
 	UserCmd.AddCommand(getUserCmd)
@@ -130,15 +90,6 @@ func init() {
 	verifyCmd.Flags().StringVar(&token, "token", token, "Verification Token")
 
 	forgotLoginCmd.Flags().StringVar(&email, "email", email, "Email ID")
-
-	pwdSetCmd.Flags().StringVar(&token, "token", token, "Verification Token")
-	pwdSetCmd.Flags().StringVar(&password, "password", password, "Password")
-
-	pwdChangeCmd.Flags().StringVar(&username, "name", username, "Account Name")
-	pwdChangeCmd.Flags().StringVar(&password, "password", password, "Current Password")
-	pwdChangeCmd.Flags().StringVar(&newPwd, "new-password", newPwd, "New Password")
-
-	pwdResetCmd.Flags().StringVar(&username, "name", username, "Account Name")
 
 	listUserCmd.Flags().BoolP("quiet", "q", false, "Only display User Name")
 
@@ -223,95 +174,6 @@ func forgotLogin(amp *cli.AMP, cmd *cobra.Command) (err error) {
 		return
 	}
 	manager.printf(colSuccess, "Your login name has been sent to the address: %s", email)
-	return nil
-}
-
-// pwd validates the input command line arguments and performs password-related operations
-// by invoking the corresponding rpc/storage method
-func pwd() (err error) {
-	manager.printf(colWarn, "Choose a command for password operation.\nUse amp account password -h for help.")
-	return nil
-}
-
-// pwdReset validates the input command line arguments and resets password of an account
-// by invoking the corresponding rpc/storage method
-func pwdReset(amp *cli.AMP, cmd *cobra.Command) (err error) {
-	if cmd.Flag("name").Changed {
-		username = cmd.Flag("name").Value.String()
-	} else {
-		fmt.Print("username: ")
-		username = getName()
-	}
-
-	request := &account.PasswordResetRequest{
-		Name: username,
-	}
-	accClient := account.NewAccountClient(amp.Conn)
-	_, err = accClient.PasswordReset(context.Background(), request)
-	if err != nil {
-		manager.fatalf(grpc.ErrorDesc(err))
-		return
-	}
-	manager.printf(colSuccess, "Hi %s! Please check your email to complete the password reset process.", username)
-	return nil
-}
-
-// pwdChange validates the input command line arguments and changes existing password of an account
-// by invoking the corresponding rpc/storage method
-func pwdChange(amp *cli.AMP, cmd *cobra.Command) (err error) {
-	fmt.Println("Enter your current password.")
-	if cmd.Flag("password").Changed {
-		password = cmd.Flag("password").Value.String()
-	} else {
-		password = getPassword()
-	}
-	fmt.Println("Enter new password.")
-	if cmd.Flag("new-password").Changed {
-		newPwd = cmd.Flag("new-password").Value.String()
-	} else {
-		newPwd = getPassword()
-	}
-
-	request := &account.PasswordChangeRequest{
-		ExistingPassword: password,
-		NewPassword:      newPwd,
-	}
-	accClient := account.NewAccountClient(amp.Conn)
-	_, err = accClient.PasswordChange(context.Background(), request)
-	if err != nil {
-		manager.fatalf(grpc.ErrorDesc(err))
-		return
-	}
-	manager.printf(colSuccess, "Your password change has been successful.")
-	return nil
-}
-
-// pwdSet validates the input command line arguments and sets password of an account
-// by invoking the corresponding rpc/storage method
-func pwdSet(amp *cli.AMP, cmd *cobra.Command) (err error) {
-	if cmd.Flag("token").Changed {
-		token = cmd.Flag("token").Value.String()
-	} else {
-		token = getToken()
-	}
-	fmt.Println("Enter new password.")
-	if cmd.Flag("password").Changed {
-		password = cmd.Flag("password").Value.String()
-	} else {
-		password = getPassword()
-	}
-
-	request := &account.PasswordSetRequest{
-		Token:    token,
-		Password: password,
-	}
-	accClient := account.NewAccountClient(amp.Conn)
-	_, err = accClient.PasswordSet(context.Background(), request)
-	if err != nil {
-		manager.fatalf(grpc.ErrorDesc(err))
-		return
-	}
-	manager.printf(colSuccess, "Your password set has been successful.")
 	return nil
 }
 
