cdc25acc899ced5a53f9f900a883ec242e11fe8a
Tony Pujals <tony@subfuzion.com>
2017-03-16 11:31:53 -0700

    Revert "Revert "vendor update for account-backend""
    This reverts commit 7de669e82dd89b0437195a0373368dca732b3dd9.

---
+++ vendor/github.com/ory-am/ladon/condition_string_equal.go
@@ -0,0 +1,20 @@
+package ladon
+
+// StringEqualCondition is a condition which is fulfilled if the given
+// string value is the same as specified in StringEqualCondition
+type StringEqualCondition struct {
+	Equals string `json:"equals"`
+}
+
+// Fulfills returns true if the given value is a string and is the
+// same as in StringEqualCondition.Equals
+func (c *StringEqualCondition) Fulfills(value interface{}, _ *Request) bool {
+	s, ok := value.(string)
+
+	return ok && s == c.Equals
+}
+
+// GetName returns the condition's name.
+func (c *StringEqualCondition) GetName() string {
+	return "StringEqualCondition"
+}
