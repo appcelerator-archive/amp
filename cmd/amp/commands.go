256b7a24638c0d5976fd33c68e4092d19e4a181a
Tony Pujals <tony@subfuzion.com>
2017-03-30 03:43:47 -0700

    [refactor cli] add --version flag

--- cmd/amp/commands.go
+++ cmd/amp/commands.go
@@ -6,19 +6,34 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type Opts struct {
+	version bool
+}
+
 // newRootCommand returns a new instance of the amp cli root command.
 func newRootCommand(c cli.Interface) *cobra.Command {
+	opts := &Opts{}
+
 	cmd := &cobra.Command{
-		Use:               "amp [OPTIONS] COMMAND [ARG...]",
-		Short:             "Deploy, manage, and monitor container stacks and functions.",
-		SilenceUsage:      true,
-		SilenceErrors:     true,
-		Example:           "amp version",
+		Use:           "amp [OPTIONS] COMMAND [ARG...]",
+		Short:         "Deploy, manage, and monitor container stacks and functions.",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Example:       "amp version",
 		Run: func(cmd *cobra.Command, args []string) {
-			c.Console().Println(cmd.UsageString())
+			if opts.version {
+				showVersion()
+				return
+			}
+			cmd.SetOutput(c.Err())
+			cmd.HelpFunc()(cmd, args)
 		},
 	}
 	cli.SetupRootCommand(cmd)
+
+	flags := cmd.Flags()
+	flags.BoolVarP(&opts.version, "version", "v", false, "Print version information and quit")
+
 	cmd.SetOutput(c.Out())
 	addCommands(cmd, c)
 	return cmd
