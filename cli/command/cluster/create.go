77d10123d7e0a4f00e7123984aabb775841dadcc
Tony Pujals <tony@subfuzion.com>
2017-03-31 10:19:40 -0700

    add cluster create --workers option

--- cli/command/cluster/create.go
+++ cli/command/cluster/create.go
@@ -5,13 +5,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type createOpts struct {
+	managers int
+	workers int
+}
+
 var (
 	createArgs = []string{"-p", "docker"}
+	opts = &createOpts{}
 )
 
 // NewCreateCommand returns a new instance of the create command for bootstrapping a local development cluster.
 func NewCreateCommand(c cli.Interface) *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "create",
 		Short: "Create a local amp cluster",
 		Long:  `The create command initializes a local amp cluster.`,
@@ -19,6 +25,11 @@ func NewCreateCommand(c cli.Interface) *cobra.Command {
 			return create(c, args)
 		},
 	}
+
+	flags := cmd.Flags()
+	flags.IntVarP(&opts.workers, "workers", "w", 2, "The initial number of worker nodes")
+
+	return cmd
 }
 
 func create(c cli.Interface, args []string) error {
