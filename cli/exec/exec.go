a3c21d18739f0b24ca4ae59deb2ad9b9fe8244c1
Tony Pujals <tony@subfuzion.com>
2017-04-01 14:00:04 -0700

    add cli exec helper

---
+++ cli/exec/exec.go
@@ -0,0 +1,49 @@
+package exec
+
+import (
+	"bufio"
+	"os/exec"
+
+	"github.com/appcelerator/amp/cli"
+)
+
+// Command is a helper to exec an os command using the streams configured for the cli.
+func Command(c cli.Interface, name string, args []string) error {
+	proc := exec.Command(name, args...)
+	stdout, err := proc.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := proc.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	outscanner := bufio.NewScanner(stdout)
+	go func() {
+		for outscanner.Scan() {
+			c.Console().Println(outscanner.Text())
+		}
+	}()
+	errscanner := bufio.NewScanner(stderr)
+	go func() {
+		for errscanner.Scan() {
+			c.Console().Println(errscanner.Text())
+		}
+	}()
+
+	err = proc.Start()
+	if err != nil {
+		return err
+	}
+
+	err = proc.Wait()
+	if err != nil {
+		// Just pass along the information that the process exited with a failure;
+		// whatever error information it displayed is what the user will see.
+		return err
+
+	}
+
+	return nil
+}
