3c217bb826d4f61cb49af02e13ac89336b0479ac
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-04-07 15:47:13 -0700

    Fix for #917 #918 #920

--- cli/command/team/member/list.go
+++ cli/command/team/member/list.go
@@ -2,7 +2,6 @@ package member
 
 import (
 	"fmt"
-	"os"
 	"text/tabwriter"
 
 	"github.com/appcelerator/amp/api/rpc/account"
@@ -66,8 +65,8 @@ func listTeamMem(c cli.Interface, cmd *cobra.Command) error {
 		}
 		return nil
 	}
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 0, ' ', 0)
-	fmt.Fprintln(w, "TEAM\tROLE\t")
+	w := tabwriter.NewWriter(c.Out(), 0, 0, cli.Padding, ' ', 0)
+	fmt.Fprintln(w, "TEAM\tROLE")
 	for _, team := range reply.Team.Members {
 		fmt.Fprintf(w, "%s\t%s\n", team.Name, team.Role)
 	}
