4c11a63ef066c42dd343f072652ef207bf46baa9
freignat91 <freignat@axway.com>
2017-04-13 14:12:35 -0700

    update stack service definition/implementation

--- api/rpc/stack/stack.go
+++ api/rpc/stack/stack.go
@@ -1,8 +1,19 @@
 package stack
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
+	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	"github.com/appcelerator/amp/pkg/docker/docker/stack"
+	"github.com/docker/docker/cli/command"
+	cliflags "github.com/docker/docker/cli/flags"
 	"github.com/golang/protobuf/ptypes/empty"
 	"golang.org/x/net/context"
 )
@@ -15,25 +26,74 @@ type Server struct {
 func (s *Server) Deploy(ctx context.Context, in *DeployRequest) (*DeployReply, error) {
 	log.Println("[stack] Deploy", in.String())
 
-	log.Println(in.Name)
-	log.Println(string(in.Compose))
-
-	log.Println("[stack] Success: created stack")
-	return &DeployReply{}, nil
+	r, w, _ := os.Pipe()
+	dockerCli := command.NewDockerCli(os.Stdin, w, w)
+	opts := cliflags.NewClientOptions()
+	if err := dockerCli.Initialize(opts); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "%v", fmt.Errorf("error in cli initialize: %v", err))
+	}
+	fileName := fmt.Sprintf("/tmp/%d-%s.yml", time.Now().UnixNano(), in.Name)
+	if err := ioutil.WriteFile(fileName, []byte(in.Compose), 0666); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "%v", err)
+	}
+	deployOpt := stack.NewDeployOptions(in.Name, fileName, true)
+	if err := stack.RunDeploy(dockerCli, deployOpt); err != nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	outs := strings.Replace(string(out), "docker", "amp", -1)
+	ans := &DeployReply{
+		Answer: string(outs),
+	}
+	return ans, nil
 }
 
 // List implements stack.Server
 func (s *Server) List(ctx context.Context, in *ListRequest) (*ListReply, error) {
 	log.Println("[stack] List", in.String())
 
-	log.Println("[stack] Success: list")
-	return &ListReply{}, nil
+	r, w, _ := os.Pipe()
+	dockerCli := command.NewDockerCli(os.Stdin, w, os.Stderr)
+	opts := cliflags.NewClientOptions()
+	if err := dockerCli.Initialize(opts); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "%v", fmt.Errorf("error in cli initialize: %v", err))
+	}
+	listOpt := stack.NewListOptions()
+	if err := stack.RunList(dockerCli, listOpt); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "%v", err)
+	}
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	outs := strings.Replace(string(out), "docker", "amp", -1)
+	ans := &ListReply{
+		Answer: string(outs),
+	}
+	return ans, nil
 }
 
 // Remove implements stack.Server
 func (s *Server) Remove(ctx context.Context, in *RemoveRequest) (*empty.Empty, error) {
 	log.Println("[stack] Remove", in.String())
 
+	_, w, _ := os.Pipe()
+	dockerCli := command.NewDockerCli(os.Stdin, w, w)
+	opts := cliflags.NewClientOptions()
+	if err := dockerCli.Initialize(opts); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "%v", fmt.Errorf("error in cli initialize: %v", err))
+	}
+	rmOpt := stack.NewRemoveOptions([]string{in.Id})
+	if err := stack.RunRemove(dockerCli, rmOpt); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "%v", err)
+	}
+	w.Close()
+	//out, _ := ioutil.ReadAll(r)
+	//outs := strings.Replace(string(out), "docker", "amp", -1)
+	/*
+		ans := &StackReply{
+			Answer: string(outs),
+		}
+	*/
 	log.Println("[stack] Success: removed", in.Id)
 	return &empty.Empty{}, nil
 }
