5856ec80c0e9715d7dfe7403365661c0cd319ec4
freignat91 <freignat@axway.com>
2017-04-13 15:12:55 -0700

    implement up stack remove cmd

--- cli/command/stack/remove.go
+++ cli/command/stack/remove.go
@@ -1,17 +1,48 @@
 package stack
 
 import (
+	"context"
+	"errors"
+
+	"github.com/appcelerator/amp/api/rpc/stack"
 	"github.com/appcelerator/amp/cli"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
-// NewStackCommand returns a new instance of the stack command.
+type removeOpts struct {
+	name string
+}
+
+var (
+	ropts = &removeOpts{}
+)
+
+// NewRemoveCommand returns a new instance of the stack command.
 func NewRemoveCommand(c cli.Interface) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "remove",
-		Short:   "Remove a deployed stack",
-		PreRunE: cli.NoArgs,
-		RunE:    c.ShowHelp,
+		Use:   "remove STACKNAME",
+		Short: "Remove a deployed stack",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ropts.name = args[0]
+			return remove(c)
+		},
 	}
 	return cmd
+}
+
+func remove(c cli.Interface) error {
+	c.Console().Printf("Remove stack: %s", opts.name)
+
+	req := &stack.RemoveRequest{
+		Id: ropts.name,
+	}
+
+	client := stack.NewStackClient(c.ClientConn())
+	reply, err := client.Remove(context.Background(), req)
+	if err != nil {
+		return errors.New(grpc.ErrorDesc(err))
+	}
+	c.Console().Println(reply)
+	return nil
 }
