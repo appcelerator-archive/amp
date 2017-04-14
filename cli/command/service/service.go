f0187f2b754996892eab1dbce931a172884c234f
freignat91 <freignat@axway.com>
2017-04-14 14:17:02 -0700

    add amp service logs

---
+++ cli/command/service/service.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"github.com/appcelerator/amp/cli"
+	"github.com/spf13/cobra"
+)
+
+// NewServiceCommand returns a new instance of the service command.
+func NewServiceCommand(c cli.Interface) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "service",
+		Short:   "Service management operations",
+		PreRunE: cli.NoArgs,
+		RunE:    c.ShowHelp,
+	}
+	cmd.AddCommand(NewServiceLogsCommand(c))
+	return cmd
+}
