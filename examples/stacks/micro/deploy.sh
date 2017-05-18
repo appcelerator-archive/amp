98f98c17646734c50a942523e7301d8a7cc2be29
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-05-18 11:00:53 -0700

    Remove non-working stacks examples

--- examples/stacks/micro/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -o errexit
-amp -s localhost stack rm micro || true
-docker build -t 127.0.0.1:5000/examples/micro .
-docker push 127.0.0.1:5000/examples/micro
-amp -s localhost stack deploy -c stack.yml micro
-
--- examples/stacks/websocket/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -o errexit
-amp -s localhost stack rm websocket || true
-docker build -t 127.0.0.1:5000/examples/ws-bash-server server
-docker build -t 127.0.0.1:5000/examples/ws-bash-web web
-docker push 127.0.0.1:5000/examples/ws-bash-server
-docker push 127.0.0.1:5000/examples/ws-bash-web
-amp -s localhost stack deploy -c stack.yml websocket
