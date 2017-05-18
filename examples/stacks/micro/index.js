1a7b6968f1a96e8757a6ba9a4b78780e7ccb8c03
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-05-18 11:00:53 -0700

    Remove non-working stacks examples

--- examples/stacks/micro/index.js
+++
@@ -1,8 +0,0 @@
-const { send } = require('micro')
-const sleep = require('then-sleep')
-
-module.exports = async function (req, res) {
-  console.log(`${req.method} ${req.url}`)
-  await sleep(500)
-  send(res, 200, `[${req.method} ${req.url}] hello.`)
-}
