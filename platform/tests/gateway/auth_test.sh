d7b1c859896031a7e3ca77ed6682264dff7e6931
qube <bquenin@axway.com>
2017-05-31 11:27:54 -0700

    update

--- platform/tests/gateway/auth_test.sh
+++ platform/tests/gateway/auth_test.sh
@@ -4,4 +4,4 @@ amp="amp -s localhost"
 $amp user signup --name user --password password --email email@user.amp
 $amp login --name user --password password
 TOKEN=$(cat ~/.config/amp/localhost.credentials)
-curl -k --header "Authorization: amp $TOKEN" https://gw.local.atomiq.io/v1/stacks | grep "{}"
+curl -kvs -H "Authorization: amp $TOKEN" https://gw.local.appcelerator.io/v1/stacks | grep "{}"
