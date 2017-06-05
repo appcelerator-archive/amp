a0556fbf56e85c1d13fbaf6fe2c664183f20eac6
Nicolas Degory <ndegory@axway.com>
2017-06-05 14:38:09 -0700

    fix cluster status and destroy command in CLI

--- cli/command/cluster/remove.go
+++ cli/command/cluster/remove.go
@@ -7,19 +7,31 @@ import (
 
 // NewRemoveCommand returns a new instance of the remove command for destroying a cluster.
 func NewRemoveCommand(c cli.Interface) *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:     "rm",
 		Aliases: []string{"remove", "destroy"},
 		Short:   "Destroy an amp cluster",
 		PreRunE: cli.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return remove(c)
+			return remove(c, cmd)
 		},
 	}
+	flags := cmd.Flags()
+	flags.StringVarP(&opts.tag, "tag", "t", "0.10.1", "Specify tag for bootstrap images (default is '0.10.1', use 'local' for development)")
+	flags.StringVar(&opts.provider, "provider", "local", "Cluster provider")
+	flags.StringVar(&opts.name, "name", "", "Cluster Label")
+	return cmd
 }
 
-func remove(c cli.Interface) error {
+func remove(c cli.Interface, cmd *cobra.Command) error {
+	// This is a map from cli cluster flag name to bootstrap script flag name
+	m := map[string]string{
+		"provider": "-t",
+		"tag":      "-T",
+	}
 	// TODO: only supporting local cluster management for this release
-	args := []string{"bootstrap/bootstrap", "-d", DefaultLocalClusterID}
-	return queryCluster(c, args, nil)
+	args := []string{"bin/deploy", "-d"}
+	args = reflag(cmd, m, args)
+	env := map[string]string{"TAG": opts.tag}
+	return queryCluster(c, args, env)
 }
