8580e56c4b8d37fe6a340b318afe1203fbf26289
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-06-05 15:10:48 -0700

    display config file, if used

--- platform/tests/config/config_cmd_test.sh
+++ platform/tests/config/config_cmd_test.sh
@@ -2,25 +2,25 @@
 
 # server address passed as command-line argument
 test_cli_config() {
-  echo $(amp -s localhost config) | grep -E "Server:[[:space:]]+localhost"
+  echo $(amp -s localhost config) | grep -Eq "Configuration file: none\s+[[:alpha:][:space:]]+: Server:\s+localhost"
 }
 
-# server address passed as command-line argument
+# server address passed in local config
 test_local_config() {
   mkdir -p $PWD/.amp
   echo "Server: LOCAL" > $PWD/.amp/amp.yml
-  echo $(amp config) | grep -E "Server:[[:space:]]+LOCAL"
+  echo $(amp config) | grep -Eq "Configuration file: $PWD/.amp/amp.yml\s+[[:alpha:][:space:]]+: Server:\s+LOCAL"
 }
 
 test_local_cleanup() {
    rm -Rf $PWD/.amp
 }
 
-# server address passed as command-line argument
+# server address passed in home config
 test_home_config() {
   mkdir -p $HOME/.config/amp
   echo "Server: HOME" > $HOME/.config/amp/amp.yml
-  echo $(amp config) | grep -E "Server:[[:space:]]+HOME"
+  echo $(amp config) | grep -Eq "Configuration file: $HOME/.config/amp/amp.yml\s+[[:alpha:][:space:]]+: Server:\s+HOME"
 }
 
 test_home_cleanup() {
