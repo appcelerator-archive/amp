9c335b2f415dfaa9e32c20e4d43390840ae54602
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-06-07 16:34:00 -0700

    add service scale command to CLI

---
+++ cli/command/service/scale.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/appcelerator/amp/api/rpc/service"
+	"github.com/appcelerator/amp/cli"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+)
+
+type scaleOptions struct {
+	service  string
+	replicas uint64
+}
+
+// NewServiceScaleCommand returns a new instance of the service scale command
+func NewServiceScaleCommand(c cli.Interface) *cobra.Command {
+	opts := scaleOptions{}
+	cmd := &cobra.Command{
+		Use:     "scale [OPTIONS]",
+		Short:   "Scale a replicated service",
+		PreRunE: cli.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return scale(c, cmd, opts)
+		},
+	}
+	flags := cmd.Flags()
+	flags.StringVar(&opts.service, "service", "", "Service ID or name")
+	flags.Uint64VarP(&opts.replicas, "replicas", "r", 0, "Number of replicas")
+
+	return cmd
+}
+
+func scale(c cli.Interface, cmd *cobra.Command, opts scaleOptions) error {
+	if !cmd.Flag("service").Changed {
+		opts.service = c.Console().GetInput("service id or name")
+	}
+	if !cmd.Flag("replicas").Changed {
+		rep, err := strconv.Atoi(c.Console().GetInput("replicas"))
+		if err != nil {
+			return fmt.Errorf("%s", err.Error())
+		}
+		opts.replicas = uint64(rep)
+	}
+
+	conn := c.ClientConn()
+	client := service.NewServiceClient(conn)
+	request := &service.ServiceScaleRequest{
+		ServiceId:      opts.service,
+		ReplicasNumber: opts.replicas,
+	}
+	if _, err := client.ScaleService(context.Background(), request); err != nil {
+		return fmt.Errorf("%s", grpc.ErrorDesc(err))
+	}
+	c.Console().Println("Service", opts.service, "has been scaled to", opts.replicas, "replicas.")
+	return nil
+}
