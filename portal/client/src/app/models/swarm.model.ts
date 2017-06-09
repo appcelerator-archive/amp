696aaa7b9b0a526709bdd3a02d1e7035e83be7ec
Francois <freignat91@users.noreply.github.com>
2017-06-09 12:33:03 -0700

    Remove-portal (#1337)
    * remove-portal
    
    * update portal stack
    
    * update deploy
    
    * update portal stack label

--- portal/client/src/app/models/swarm.model.ts
+++
@@ -1,9 +0,0 @@
-export class Swarm {
-  name: string
-  organization: string
-
-  constructor(name: string, organization: string) {
-    this.name = name
-    this.organization = organization
-  }
-}
