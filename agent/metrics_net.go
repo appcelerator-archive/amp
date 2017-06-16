a5f5f94eb7accbf3d85e4d0df88fb8af8f503fbe
Francois <freignat91@users.noreply.github.com>
2017-06-16 13:59:20 -0700

    updte io-net agent computation (#1395)

--- agent/metrics_net.go
+++ agent/metrics_net.go
@@ -10,27 +10,27 @@ import (
 // NetStats net stats
 type NetStats struct {
 	Time      time.Time
-	RxBytes   uint64
-	RxDropped uint64
-	RxErrors  uint64
-	RxPackets uint64
-	TxBytes   uint64
-	TxDropped uint64
-	TxErrors  uint64
-	TxPackets uint64
+	RxBytes   float64
+	RxDropped float64
+	RxErrors  float64
+	RxPackets float64
+	TxBytes   float64
+	TxDropped float64
+	TxErrors  float64
+	TxPackets float64
 }
 
 // NetStatsDiff diff between two IOStats
 type NetStatsDiff struct {
-	Duration  int64
-	RxBytes   int64
-	RxDropped int64
-	RxErrors  int64
-	RxPackets int64
-	TxBytes   int64
-	TxDropped int64
-	TxErrors  int64
-	TxPackets int64
+	Duration  float64
+	RxBytes   float64
+	RxDropped float64
+	RxErrors  float64
+	RxPackets float64
+	TxBytes   float64
+	TxDropped float64
+	TxErrors  float64
+	TxPackets float64
 }
 
 // publish one net metrics event
@@ -45,46 +45,46 @@ func (a *Agent) setNetMetrics(data *ContainerData, statsData *types.StatsJSON, e
 		return
 	}
 	data.previousNetStats = net
-	entry.Net.TotalBytes += diff.RxBytes + diff.TxBytes
-	entry.Net.RxBytes += diff.RxBytes
-	entry.Net.RxDropped += diff.RxDropped
-	entry.Net.RxErrors += diff.RxErrors
-	entry.Net.RxPackets += diff.RxPackets
-	entry.Net.TxBytes += diff.TxBytes
-	entry.Net.TxDropped += diff.TxDropped
-	entry.Net.TxErrors += diff.TxErrors
-	entry.Net.TxPackets += diff.TxPackets
+	entry.Net.TotalBytes += int64(diff.RxBytes + diff.TxBytes)
+	entry.Net.RxBytes += int64(diff.RxBytes)
+	entry.Net.RxDropped += int64(diff.RxDropped)
+	entry.Net.RxErrors += int64(diff.RxErrors)
+	entry.Net.RxPackets += int64(diff.RxPackets)
+	entry.Net.TxBytes += int64(diff.TxBytes)
+	entry.Net.TxDropped += int64(diff.TxDropped)
+	entry.Net.TxErrors += int64(diff.TxErrors)
+	entry.Net.TxPackets += int64(diff.TxPackets)
 }
 
 // create a new net stats
 func (a *Agent) newNetStats(stats *types.StatsJSON) *NetStats {
 	var net = &NetStats{Time: stats.Read}
 	for _, netStats := range stats.Networks {
-		net.RxBytes += netStats.RxBytes
-		net.RxDropped += netStats.RxDropped
-		net.RxErrors += netStats.RxErrors
-		net.RxPackets += netStats.RxPackets
-		net.TxBytes += netStats.TxBytes
-		net.TxDropped += netStats.TxDropped
-		net.TxErrors += netStats.TxErrors
-		net.TxPackets += netStats.TxPackets
+		net.RxBytes += float64(netStats.RxBytes)
+		net.RxDropped += float64(netStats.RxDropped)
+		net.RxErrors += float64(netStats.RxErrors)
+		net.RxPackets += float64(netStats.RxPackets)
+		net.TxBytes += float64(netStats.TxBytes)
+		net.TxDropped += float64(netStats.TxDropped)
+		net.TxErrors += float64(netStats.TxErrors)
+		net.TxPackets += float64(netStats.TxPackets)
 	}
 	return net
 }
 
 // create a new net diff computing difference between two net stats
 func (a *Agent) newNetDiff(newNet *NetStats, previousNet *NetStats) *NetStatsDiff {
-	diff := &NetStatsDiff{Duration: int64(newNet.Time.Sub(previousNet.Time).Seconds())}
+	diff := &NetStatsDiff{Duration: float64(newNet.Time.Sub(previousNet.Time).Minutes())}
 	if diff.Duration <= 0 {
 		return nil
 	}
-	diff.RxBytes = int64(newNet.RxBytes-previousNet.RxBytes) / diff.Duration
-	diff.RxDropped = int64(newNet.RxDropped-previousNet.RxDropped) / diff.Duration
-	diff.RxErrors = int64(newNet.RxErrors-previousNet.RxErrors) / diff.Duration
-	diff.RxPackets = int64(newNet.RxPackets-previousNet.RxPackets) / diff.Duration
-	diff.TxBytes = int64(newNet.TxBytes-previousNet.TxBytes) / diff.Duration
-	diff.TxDropped = int64(newNet.TxDropped-previousNet.TxDropped) / diff.Duration
-	diff.TxErrors = int64(newNet.TxErrors-previousNet.TxErrors) / diff.Duration
-	diff.TxPackets = int64(newNet.TxPackets-previousNet.TxPackets) / diff.Duration
+	diff.RxBytes = (newNet.RxBytes - previousNet.RxBytes) / diff.Duration
+	diff.RxDropped = (newNet.RxDropped - previousNet.RxDropped) / diff.Duration
+	diff.RxErrors = (newNet.RxErrors - previousNet.RxErrors) / diff.Duration
+	diff.RxPackets = (newNet.RxPackets - previousNet.RxPackets) / diff.Duration
+	diff.TxBytes = (newNet.TxBytes - previousNet.TxBytes) / diff.Duration
+	diff.TxDropped = (newNet.TxDropped - previousNet.TxDropped) / diff.Duration
+	diff.TxErrors = (newNet.TxErrors - previousNet.TxErrors) / diff.Duration
+	diff.TxPackets = (newNet.TxPackets - previousNet.TxPackets) / diff.Duration
 	return diff
 }
