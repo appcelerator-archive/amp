05268d68ff53848e9d548e8204f5aec6c6af78e5
Tony Pujals <tony@subfuzion.com>
2017-06-18 20:27:26 -0700

    add initial support for cluster plugins

--- cli/command/cluster/cluster.go
+++ cli/command/cluster/cluster.go
@@ -14,6 +14,7 @@ type clusterOpts struct {
 	tag           string
 	registration  string
 	notifications bool
+	options       map[string]string
 }
 
 var (
@@ -25,6 +26,7 @@ var (
 		tag:           "latest",
 		registration:  configuration.RegistrationDefault,
 		notifications: true,
+		options: map[string]string{},
 	}
 )
 
