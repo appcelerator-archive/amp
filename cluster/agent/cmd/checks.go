ce6ec9e8529872e03c301574618d300de539b719
Tony Pujals <tony@subfuzion.com>
2017-07-05 17:21:46 -0700

    refactor agent commands

--- cluster/agent/cmd/checks.go
+++ cluster/agent/cmd/checks.go
@@ -15,6 +15,19 @@ type CheckOptions struct {
 
 var checksOpts = &CheckOptions{}
 
+func NewChecksCommand() *cobra.Command {
+	checkCmd := &cobra.Command{
+		Use:   "check",
+		Short: "Run validation tests on the cluster",
+		Run:   checks,
+	}
+	checkCmd.Flags().BoolVar(&checksOpts.version, "version", false, "check Docker version")
+	checkCmd.Flags().BoolVar(&checksOpts.scheduling, "scheduling", false, "check Docker service scheduling")
+	checkCmd.Flags().BoolVarP(&checksOpts.all, "all", "a", false, "all tests")
+
+	return checkCmd
+}
+
 func checks(cmd *cobra.Command, args []string) {
 	if checksOpts.version || checksOpts.all {
 		out, err := adm.VerifyDockerVersion()
