601cbcc4d39d5360cdc4aabf37cda0f906ca5d12
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-07-12 12:06:26 -0700

    Remove org reference in commands

--- cli/command/stack/list.go
+++ cli/command/stack/list.go
@@ -49,9 +49,9 @@ func list(c cli.Interface, opts listStackOptions) error {
 		return nil
 	}
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, cli.Padding, ' ', 0)
-	fmt.Fprintln(w, "ID\tNAME\tSERVICES\tFAILED SERVICES\tSTATUS\tOWNER\tORGANIZATION")
+	fmt.Fprintln(w, "ID\tNAME\tSERVICES\tFAILED SERVICES\tSTATUS\tOWNER")
 	for _, entry := range reply.Entries {
-		fmt.Fprintf(w, "%s\t%s\t%d/%d\t%d\t%s\t%s\t%s\n", entry.Stack.Id, entry.Stack.Name, entry.RunningServices, entry.TotalServices, entry.FailedServices, entry.Status, entry.Stack.Owner.User, entry.Stack.Owner.Organization)
+		fmt.Fprintf(w, "%s\t%s\t%d/%d\t%d\t%s\t%s\n", entry.Stack.Id, entry.Stack.Name, entry.RunningServices, entry.TotalServices, entry.FailedServices, entry.Status, entry.Stack.Owner.User)
 	}
 	w.Flush()
 	return nil
