cd7cddbbd4c2fb6fa4c13be07eb0d20bc07e6fe6
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-07-25 10:32:04 -0700

    Filter tasks with -a option

--- platform/tests/service/ps/ps_test.sh
+++ platform/tests/service/ps/ps_test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 amp -k service ps pinger_pinger 2>/dev/null | grep -o -w -E -q '[[:alnum:]]{25}'
+
+amp -k service ps pinger_pinger 2>/dev/null | grep -Evq 'SHUTDOWN'
+
+amp -k service ps pinger_pinger -a 2>/dev/null | grep -E -i 'RUNNING|SHUTDOWN'
