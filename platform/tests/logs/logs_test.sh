9c6f50cd300ebb0e3bb5e4321ece293c560cac7e
JosephGJ <jgraham-jones@axway.com>
2017-08-01 14:01:43 -0700

    updating smoke tests

--- platform/tests/logs/logs_test.sh
+++ platform/tests/logs/logs_test.sh
@@ -1,3 +1,53 @@
 #!/bin/bash
 
-amp -k logs -m | grep -q "timestamp:"
+test_logs() {
+  while true
+  do
+     if amp -k logs | grep -q "pinger_pinger.*listening on :3000"
+     then
+             break
+     fi
+     sleep 1
+  done
+}
+
+test_logs_container() {
+  amp -k logs --container "pinger_pinger.1" | grep -q "pinger_pinger.1.*listening on :3000"
+}
+
+test_logs_include() {
+  amp -k logs -i | grep -q "amp_"
+}
+
+test_logs_metadata() {
+  amp -k logs -m | grep -q ".*container_name:.*pinger_pinger.*container_state.*running.*"
+}
+
+test_logs_msg() {
+  amp -k logs --msg "listening on :3000" | grep -q "pinger_pinger.*listening on :3000.*"
+}
+
+test_logs_node() {
+  nodeid=$(docker node inspect self --format "{{.ID}}")
+  amp -k logs --node $nodeid | grep -q "pinger_pinger.*listening on :3000.*"
+}
+
+test_logs_number() {
+  amp -k logs -n 2 | wc -l | grep -q "2"
+}
+
+test_logs_raw() {
+  amp -k logs -r | grep -q ".*listening on :3000"
+}
+
+test_logs_regexp() {
+  amp -k logs --regexp --msg ".*listening.*" | grep -q "pinger_pinger.*listening on :3000.*"
+}
+
+test_logs_since() {
+  amp -k logs --since 1 | grep -q "pinger_pinger.*listening on :3000"
+}
+
+test_logs_stack() {
+  amp -k logs --stack pinger | grep -q "pinger_pinger.*listening on :3000"
+}
