22e85bd6b3cfe9a93c197d87281cf510eca9e88f
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-08-03 10:49:54 -0700

    Updated smoke tests for amp settings cmd

--- platform/tests/secrets/secret_test.sh
+++ platform/tests/secrets/secret_test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 amp -k secret create TEST platform/tests/secrets/create/test | grep -o -w -E -q '[[:alnum:]]{25}'
+
+amp -k secret ls | grep -q 'TEST'
+
+amp -k secret rm TEST | grep -q 'TEST'
