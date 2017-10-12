6a2dfa79d0defdf74753008945da9027a28d8bf8
Nicolas Degory <nicolas.degory@gmail.com>
2017-10-12 13:56:27 -0700

    add metrics to pinger (#1697)

--- images/pinger/build.sh
+++ images/pinger/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# invokes make.sh to build an alpine binary, then it builds an alpine-based image with the binary
 #
-set -e
-./make.sh
 docker build -t appcelerator/pinger . \
 	&& docker tag appcelerator/pinger appcelerator/pinger:$(cat VERSION | sed 's/[:space:]*$//')
-
