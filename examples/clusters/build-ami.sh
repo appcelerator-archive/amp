1d4e74ff951f5462af23ef76d8f83df620edbd07
Nicolas Degory <ndegory@axway.com>
2017-10-24 17:32:15 -0700

    update ansible image to 2.4, resolve deprecation warnings

--- examples/clusters/build-ami.sh
+++ examples/clusters/build-ami.sh
@@ -2,7 +2,7 @@
 DIR="$(dirname $0)"
 cd "$DIR"
 DIR="$(pwd -P)"
-image="appcelerator/ansible:2.3"
+image="appcelerator/ansible:2.4"
 REGIONS="us-east-1 us-east-2 us-west-2 eu-west-1 ap-southeast-2"
 OWNER=654814900965
 IMAGE_NAME=ubuntu-xenial-docker
