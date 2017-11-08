f2ad2b2db065db89a5f590bebdce6fbcb58ed602
Neha Viswanathan <neha.pennathur@hotmail.com>
2017-11-08 10:01:37 -0800

    Service ps now displays all tasks irrespective of their status

--- tests/cli/service/ps/ps_test.sh
+++ tests/cli/service/ps/ps_test.sh
@@ -2,6 +2,4 @@
 
 amp -k service ps pinger_pinger 2>/dev/null | grep -o -w -E -q '[[:alnum:]]{25}'
 
-amp -k service ps pinger_pinger 2>/dev/null | grep -Evq 'SHUTDOWN'
-
-amp -k service ps pinger_pinger -a 2>/dev/null | grep -E -i 'RUNNING|SHUTDOWN'
+amp -k service ps pinger_pinger 2>/dev/null | grep -E -i 'RUNNING|SHUTDOWN'
