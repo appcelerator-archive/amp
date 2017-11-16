19fe1d7d35a816110f26ca45171206af2399bf93
Nicolas Degory <ndegory@axway.com>
2017-11-15 16:41:19 -0800

    prepare for 0.16.0

--- cli/command/cluster/cluster.go
+++ cli/command/cluster/cluster.go
@@ -35,7 +35,7 @@ var (
 		workers:       2,
 		provider:      "local",
 		name:          "",
-		tag:           "latest",
+		tag:           "0.16.0",
 		registration:  configuration.RegistrationDefault,
 		notifications: true,
 	}
