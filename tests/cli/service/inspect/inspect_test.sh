bde125bb910c882df19c864c58f0fc1974ea3c55
Nicolas Degory <ndegory@axway.com>
2017-11-28 09:49:29 -0800

    add --format option to CLI service inspect subcommand

--- tests/cli/service/inspect/inspect_test.sh
+++ tests/cli/service/inspect/inspect_test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-amp -k service inspect pinger_pinger 2>/dev/null | pcregrep -q "pinger"
+test_inspect() {
+  amp -k service inspect pinger_pinger 2>/dev/null | grep -q "pinger"
+}
+
+test_inspect_format() {
+  secret_name=amplifier_yml
+  secrets=$(amp -k service inspect amp_amplifier --format '{{ range .Spec.TaskTemplate.ContainerSpec.Secrets}} {{ .SecretName }} {{ end }}') || return 1
+ echo $secrets | grep -q $secret_name
+}
